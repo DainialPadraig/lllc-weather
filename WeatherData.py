ec3ac2a31f352838cfaa6c42905d7ee915edc796
DainialPadraig <daniel.stormont@gmail.com>
2018-06-26 01:24:44 -0700

    Experiment with MesoPy data

--- WeatherData.py
+++ WeatherData.py
@@ -1,5 +1,31 @@
+'''
+WeatherData
+   Retrives data from the MesoWest repository for the Living Lab and Learning
+   Center weather station and displays it on a Raspberry Pi touchscreen.
+   Hardware: Raspberry Pi 3
+             Raspberry Pi official 7" touchscreen display.
+   Software: Raspbian Jessie
+             Python 2.7
+             MesoPy 2.0.2
+             Kivy
+   Program by Dan Stormont
+   License:
+'''
+
 from MesoPy import Meso
 
+def main():
+    m = Meso(token='0ee93b568404435a9f98ead284f5fc9c')
 
-m = Meso(token='0ee93b568404435a9f98ead284f5fc9c')
-precip = m.precip(stid='e8967', start='201801010000', end='201806232359', units='precip|in')
+    current = m.latest(stid='e8967')
+    precip = m.precip(stid='e8967',
+                      start='201801010000', end='201806232359',
+                      units='precip|in')
+    print('Current station observations:')
+    print(current)
+    print('Precipitation observations since the start of the year:')
+    print(precip)
+
+if __name__ == '__main__':
+    main()
+    
