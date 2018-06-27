daea6f58cbe0dc2c8a44372c33a275399c5f73fc
Dan Stormont <dstormont@watershedmg.org>
2018-06-27 01:32:24 -0700

    Create functions for displays

--- WeatherData.py
+++ WeatherData.py
@@ -1,30 +1,51 @@
-'''
-WeatherData
-   Retrives data from the MesoWest repository for the Living Lab and Learning
-   Center weather station and displays it on a Raspberry Pi touchscreen.
-   Hardware: Raspberry Pi 3
-             Raspberry Pi official 7" touchscreen display.
-   Software: Raspbian Jessie
-             Python 2.7
-             MesoPy 2.0.2
-             Kivy
-   Program by Dan Stormont
-   License:
-'''
+# WeatherData
+#   Retrives data from the MesoWest repository for the Living Lab and Learning
+#   Center weather station and displays it on a Raspberry Pi touchscreen.
+#   Hardware: Raspberry Pi 3
+#             Raspberry Pi official 7" touchscreen display.
+#   Software: Raspbian Jessie
+#             Python 2.7
+#             MesoPy 2.0.3
+#             Kivy
+#   Program by Dan Stormont
+#   License:
+
 
 from MesoPy import Meso
 
-def main():
-    m = Meso(token='0ee93b568404435a9f98ead284f5fc9c')
+#TODO-Get rid of these global constants and variables
+MESOWEST_TOKEN = '0ee93b568404435a9f98ead284f5fc9c'    
+m = Meso(token=MESOWEST_TOKEN)
 
-    current = m.latest(stid='e8967')
-    precip = m.precip(stid='e8967',
-                      start='201801010000', end='201806232359',
-                      units='precip|in')
-    print('Current station observations:')
-    print(current)
-    print('Precipitation observations since the start of the year:')
-    print(precip)
+
+def display_latest(station_id):
+    """Display the latest weather readings from station_id."""
+    
+    print 'Most recent weather readings:'
+    print '-----------------------------'
+    print m.latest(stid=station_id,
+                   units='precip|in,speed|mph,temp|F')
+    print
+
+
+def display_precip_ytd(station_id):
+    """Display total rainfall since Jan 1 at station_id.""" 
+    print 'Total rainfall since the start of the year:'
+    print '-------------------------------------------'
+    print m.precip(stid=station_id,
+                   start='201801010000', end='201806232359',
+                   units='precip|in')
+    print
+    
+
+    
+def main():
+    """Main function for WeatherData."""
+
+    LLLC_STID = 'e8967'
+
+    display_latest(LLLC_STID)
+    display_precip_ytd(LLLC_STID)
 
 if __name__ == '__main__':
     main()
